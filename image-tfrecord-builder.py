e73147a65a965ad9bb038ebe42eb99e410b79a19
Bruno Tinen <tinen.bruno@gmail.com>
2018-06-02 20:37:22 -0300

    Settings file

--- image-tfrecord-builder.py
+++ image-tfrecord-builder.py
@@ -2,10 +2,7 @@ import numpy as np
 import tensorflow as tf
 import sys
 import cv2
-
-IMAGES_INPUT_FOLDER = "/home/bruno/Downloads/AVA_dataset/nature2/negative/"
-DATASET_DEFINITIONS = r'../../AVA_dataset/AVA.txt'
-OUTPUT_FILENAME = 'nature.tfrecords'
+from settings import app
 
 def load_image(path):
     image = cv2.imread(path)
@@ -20,13 +17,13 @@ def _int64_feature(value):
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-file = open(DATASET_DEFINITIONS)
-writer = tf.python_io.TFRecordWriter(OUTPUT_FILENAME)
+file = open(app['DATASET_DEFINITIONS'])
+writer = tf.python_io.TFRecordWriter(app['OUTPUT_FILENAME'])
 
 for line in file:
     line = line.strip().split(' ')
     try:
-        path = IMAGES_INPUT_FOLDER + line[1] + '.jpg'
+        path = app['IMAGES_INPUT_FOLDER'] + line[1] + app['IMAGES_INPUT_EXTENSION']
         image = load_image(path)
         if image is not None:
             vote_sum = 0
